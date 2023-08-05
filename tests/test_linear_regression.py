49e8624d086999d1a5322eed3f09416f55a1c821
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-08-05 23:05:27 +0200

    style: apply linter fixes

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -1,2 +1,12 @@
-def test_linear_regression() -> None:
-    assert True is True
+"""
+Unit tests for Linear Regression model
+"""
+
+from cmnemoi_learn.linear_regression import LinearRegression
+
+
+def test_predict() -> None:
+    """
+    Test `predict` againt sklearn implementation.
+    """
+    LinearRegression()
