161562add6459a2e52524c266ce4c101256907b2
Evian <icdk5b4zf@relay.firefox.com>
2023-08-06 02:49:09 +0200

    refactor: put fixtures parameters into constants

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,10 @@ import numpy as np
 from sklearn.datasets import make_regression, make_circles
 import pytest
 
+BIAS = 5
+NOISE = 2
+NUMBER_OF_FEATURES = 2
+NUMBER_OF_SAMPLES = 50
 RANDOM_STATE = 42
 
 
@@ -17,7 +21,9 @@ def regression_circle_dataset() -> np.ndarray:
     Returns:
         np.ndarray: The dataset
     """
-    return make_circles(n_samples=100, shuffle=False, random_state=RANDOM_STATE)
+    return make_circles(
+        n_samples=NUMBER_OF_SAMPLES, shuffle=False, random_state=RANDOM_STATE
+    )
 
 
 @pytest.fixture
@@ -29,9 +35,10 @@ def regression_linear_dataset() -> np.ndarray:
         np.ndarray: The dataset
     """
     return make_regression(
-        n_samples=100,
-        n_features=10,
-        n_informative=10,
+        n_samples=NUMBER_OF_SAMPLES,
+        n_features=NUMBER_OF_FEATURES,
+        n_informative=NUMBER_OF_FEATURES,
+        bias=BIAS,
         shuffle=False,
         random_state=RANDOM_STATE,
     )
@@ -46,10 +53,11 @@ def regression_linear_dataset_with_noise() -> np.ndarray:
         np.ndarray: The dataset
     """
     return make_regression(
-        n_samples=100,
-        n_features=10,
-        n_informative=10,
-        noise=2,
+        n_samples=NUMBER_OF_SAMPLES,
+        n_features=NUMBER_OF_FEATURES,
+        n_informative=NUMBER_OF_FEATURES,
+        bias=BIAS,
+        noise=NOISE,
         shuffle=False,
         random_state=RANDOM_STATE,
     )
