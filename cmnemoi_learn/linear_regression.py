a44f5e1b8985832792a5f0b57479e75d52537508
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-08-06 02:02:42 +0200

    feat: add LinearRegression .fit and .predict method

--- cmnemoi_learn/linear_regression.py
+++ cmnemoi_learn/linear_regression.py
@@ -4,6 +4,7 @@ File defining a Linear Regression model.
 
 from typing import Self
 import numpy as np
+from numpy.linalg import inv
 
 
 class LinearRegression:
@@ -15,9 +16,10 @@ class LinearRegression:
     def __init__(self) -> None:
         self.X = np.array([])
         self.y = np.array([])
+        self.theta = np.array([])
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> Self:
-        """Fit the Linear Regression model
+        """Fit the Linear Regression model with normal equations solution
 
         Args:
             X (np.ndarray): Inputs
@@ -26,12 +28,11 @@ class LinearRegression:
         Returns:
             LinearRegression: Fitted Linear Regression model.
         """
-        self.X = X
-        self.y = y
+        self.theta = inv(X.T @ X) @ (X.T @ y)
         return self
 
-    def predict(self, X: np.ndarray) -> Self:
-        """Predict new values with the Linear Regression model for the inputs given on arguments.
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """Predict new values with the Linear Regression model for the inputs given on argument.
 
         Args:
             X (np.ndarray): New inputs on which to predict.
@@ -39,5 +40,4 @@ class LinearRegression:
         Returns:
             LinearRegression: Linear Regression model used to predict.
         """
-        print(X)
-        return self
+        return X.T @ self.theta
