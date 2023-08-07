a37af61b7222347702836cc0e59c50700918a0d0
Evian <icdk5b4zf@relay.firefox.com>
2023-08-07 20:44:33 +0200

    chore: add an `AbstractClassifier`

---
+++ cmnemoi_learn/classification/abstract_classifier.py
@@ -0,0 +1,33 @@
+"""
+File defining an abstract classifier (for predicting discrete `y` values)
+"""
+
+import numpy as np
+
+from cmnemoi_learn.abstract_model import AbstractModel
+
+
+class AbstractClassifier(AbstractModel):
+    """Class defining an abstract classifier (for predicting continous `y` values).
+
+    Args:
+        AbstractModel (AbstractModel): Class defining an abstract model.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.threshold = 0.5
+
+    def score(self, X: np.ndarray, y: np.ndarray) -> float:
+        """Returns the accuracy of the model for the
+        inputs and output given on arguments.
+
+        Args:
+            X (np.ndarray): Inputs
+            y (np.ndarray): Output
+
+        Returns:
+            float: Mean Squared Error
+        """
+        y_pred = self.predict(X)
+        return 0
