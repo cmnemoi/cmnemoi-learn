7011615541d11df00cd7cd2b7721f0e0f796a5f3
Evian <icdk5b4zf@relay.firefox.com>
2023-08-07 20:44:18 +0200

    tests: add tests for logisitic regression

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,68 @@ Fixtures for unit tests
 """
 
 import numpy as np
-from sklearn.datasets import make_regression, make_friedman2
+from sklearn.datasets import (
+    make_classification,
+    make_circles,
+    make_regression,
+    make_friedman2,
+)
 import pytest
 
 BIAS = 5
 NUMBER_OF_FEATURES = 2
 NUMBER_OF_SAMPLES = 50
 RANDOM_STATE = 42
+
+
+@pytest.fixture
+def classification_circles_dataset() -> np.ndarray:
+    """Classification dataset with circles pattern (non linear)
+    `X, y = classification_circles_dataset` to use
+
+    Returns:
+        np.ndarray: The dataset
+    """
+    return make_circles(
+        n_samples=NUMBER_OF_SAMPLES, shuffle=False, random_state=RANDOM_STATE
+    )
+
+
+@pytest.fixture
+def classification_linear_dataset() -> np.ndarray:
+    """Classification dataset linearly separable
+    `X, y = classification_linear_dataset` to use
+
+    Returns:
+        np.ndarray: The dataset
+    """
+    return make_classification(
+        n_samples=NUMBER_OF_SAMPLES,
+        n_features=NUMBER_OF_FEATURES,
+        n_informative=NUMBER_OF_FEATURES,
+        n_redundant=0,
+        shuffle=False,
+        random_state=RANDOM_STATE,
+    )
+
+
+@pytest.fixture
+def classification_linear_dataset_with_small_n_big_p() -> np.ndarray:
+    """Classification dataset linearly separable with small n and big p
+    (under determined problem).
+    `X, y = classification_linear_dataset_with_small_n_big_p` to use
+
+    Returns:
+        np.ndarray: The dataset
+    """
+    return make_classification(
+        n_samples=NUMBER_OF_SAMPLES,
+        n_features=NUMBER_OF_SAMPLES + 1,
+        n_informative=NUMBER_OF_SAMPLES + 1,
+        n_redundant=0,
+        shuffle=False,
+        random_state=RANDOM_STATE,
+    )
 
 
 @pytest.fixture
