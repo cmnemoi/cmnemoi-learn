efc73d438cd37daad8c77bab809f0a9bf2e46333
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-08-10 12:21:34 +0200

    style: rename some variables

--- cmnemoi_learn/classification/logistic_regression.py
+++ cmnemoi_learn/classification/logistic_regression.py
@@ -1,7 +1,7 @@
 """
 File defining a logistic regression model.
 """
-from typing import Self
+from typing import Optional, Self
 import numpy as np
 
 from cmnemoi_learn.classification.abstract_classifier import AbstractClassifier
@@ -14,21 +14,23 @@ class LogisticRegression(AbstractClassifier):
     probability a sample x to belong to the
     `Y=1` positive class : `P(Y=1|X=x)`.
 
-    For this, it assumes `P(Y=1|X=x) = z(X.theta)`
+    For this, it assumes `P(Y=1|X=x) = z(X.weights)`
     where z is the logistic function `z(x) = (1 + exp(-x))**-1`
     """
 
-    def __init__(self) -> None:
+    def __init__(self, n_iter: int = 500, random_state: Optional[int] = None) -> None:
         super().__init__()
-        self.theta = np.array([])
+        self.n_iter = n_iter
+        self.rng = np.random.default_rng(seed=random_state)
+        self.weights = np.array([])
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> Self:
         """Fit the model.
 
-        The parameters `theta` of the model are found
+        The `weights` of the model are found
         by minimizing the negative log-likelihood of the problem
         (aka. log loss or cross entropy) :
-        `−∑ ylog(z(X.theta)) + (1-y)log(1-z(X.theta))` with
+        `−∑ ylog(z(X.weights)) + (1-y)log(1-z(X.weights))` with
         gradient descent algorithm.
 
         Args:
@@ -40,7 +42,7 @@ class LogisticRegression(AbstractClassifier):
         """
         self.X = self._get_inputs_with_bias_column(X)
         self.y = self._reshape_ndarray(y)
-        self.theta = self._gradient_descent()
+        self.weights = self._gradient_descent(n_iter=self.n_iter)
         return self
 
     def predict(self, X: np.ndarray) -> np.ndarray:
@@ -53,33 +55,34 @@ class LogisticRegression(AbstractClassifier):
             np.ndarray: Output
         """
         X = self._get_inputs_with_bias_column(X)
-        probabilities = self._logistic_function(X @ self.theta)
+        probabilities = self._logistic_function(X @ self.weights)
         return np.array(
-            [1 if probability >= self.threshold else 0 for probability in probabilities]
+            [1 if probability > self.threshold else 0 for probability in probabilities]
         )
 
-    def _gradient_descent(
-        self, n_iter: int = 1000, learning_rate: float = 0.1
-    ) -> np.ndarray:
-        """Gradient descent
+    def _gradient_descent(self, n_iter: int, learning_rate: float = 0.5) -> np.ndarray:
+        """Gradient descent algorithm.
+        Find the best `weights` for logistic regression by minimizing the log loss.
 
         Args:
-            n_iter (int, optional): _description_. Defaults to 1000.
-            learning_rate (float, optional): _description_. Defaults to 0.1.
+            n_iter (int, optional): Number of iterations.
+            learning_rate (float, optional): Learning rate. Defaults to 0.5.
 
         Returns:
-            np.ndarray: Optimized theta
+            Tuple[np.ndarray, float]: Weights and bias of the model.
         """
         n = self.X.shape[0]
-        theta = np.random.randn(self.X.shape[1], 1)
-        gradient = np.array([])
+        weights = self.rng.random((self.X.shape[1], 1))
+        weights_gradient = np.array([])
         for _ in range(n_iter):
-            gradient = (
-                (1 / n) * self.X.T @ (self._logistic_function(self.X @ theta) - self.y)
+            weights_gradient = (
+                (1 / n)
+                * self.X.T
+                @ (self._logistic_function(self.X @ weights) - self.y)
             )
-            theta = theta - learning_rate * gradient
+            weights = weights - learning_rate * weights_gradient
 
-        return theta
+        return weights
 
     def _get_inputs_with_bias_column(self, X: np.ndarray) -> np.ndarray:
         """Returns the inputs `X` with a `1`-filled bias column.
