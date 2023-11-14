82a8f31beb0320cf919f42d5d72671bdd11a6933
Evian <icdk5b4zf@relay.firefox.com>
2023-11-14 23:20:51 +0100

    feat: Add euclidian distance

--- cmnemoi_learn/metrics.py
+++ cmnemoi_learn/metrics.py
@@ -2,6 +2,20 @@
 
 import numpy as np
 
+def euclidian_distance(v_1: np.ndarray, v_2: np.ndarray) -> float:
+    """Compute Euclidian distance (L2 norm) between two vectors.
+
+    Args:
+        v_1: Vector as numpy array
+        v_2 Vector as numpy array
+
+    Returns:
+        float: Euclidian distance
+    """
+    if v_1.shape != v_2.shape:
+        raise ValueError("v_1 and v_2 should have the same shape")
+    return np.sqrt(np.sum((v_1 - v_2) ** 2))
+
 
 def manhattan_distance(v_1: np.ndarray, v_2: np.ndarray) -> float:
     """Compute Manhattan distance (L1 norm) between two vectors.
@@ -14,5 +28,5 @@ def manhattan_distance(v_1: np.ndarray, v_2: np.ndarray) -> float:
         float: Manhattan distance
     """
     if v_1.shape != v_2.shape:
-        raise ValueError("a and b should have the same shape")
+        raise ValueError("v_1 and v_2 should have the same shape")
     return np.sum(np.abs(v_1 - v_2))
--- tests/metrics/test_metrics.py
+++ tests/metrics/test_metrics.py
@@ -5,21 +5,37 @@ import numpy as np
 import pytest
 from scipy.spatial import distance
 
-from cmnemoi_learn.metrics import manhattan_distance
+from cmnemoi_learn.metrics import euclidian_distance, manhattan_distance
 
 
 def test_manhattan_distance() -> None:
     """Test for manhattan distance"""
-    a = np.random.randn(1, 10).ravel()
-    b = np.random.randn(1, 10).ravel()
+    v_1 = np.random.randn(1, 10).ravel()
+    v_2 = np.random.randn(1, 10).ravel()
 
-    assert manhattan_distance(a, b) == distance.cityblock(a, b)
+    assert manhattan_distance(v_1, v_2) == distance.cityblock(v_1, v_2)
 
 
 def test_manhattan_distance_raises_exception_if_arrays_not_same_size() -> None:
     """Test for manhattan distance"""
-    a = np.random.randn(1, 9).ravel()
-    b = np.random.randn(1, 10).ravel()
+    v_1 = np.random.randn(1, 9).ravel()
+    v_2 = np.random.randn(1, 10).ravel()
 
-    with pytest.raises(ValueError, match="a and b should have the same shape"):
-        manhattan_distance(a, b)
+    with pytest.raises(ValueError, match="v_1 and v_2 should have the same shape"):
+        manhattan_distance(v_1, v_2)
+
+def test_euclidian_distance() -> None:
+    """Test for euclidian distance"""
+    v_1 = np.random.randn(1, 10).ravel()
+    v_2 = np.random.randn(1, 10).ravel()
+
+    assert np.isclose(euclidian_distance(v_1, v_2), distance.euclidean(v_1, v_2))
+
+
+def test_euclidian_distance_raises_exception_if_arrays_not_same_size() -> None:
+    """Test for euclidian distance"""
+    v_1 = np.random.randn(1, 9).ravel()
+    v_2 = np.random.randn(1, 10).ravel()
+
+    with pytest.raises(ValueError, match="v_1 and v_2 should have the same shape"):
+        euclidian_distance(v_1, v_2)
